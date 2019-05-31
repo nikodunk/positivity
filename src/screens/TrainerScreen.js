c5fb67850e2a0fb6f77eb3e8b2fea4c306d1982d
Nikolai <n.dunkel@gmail.com>
2019-05-31 12:30:21 -0700

    random question working

--- src/screens/TrainerScreen.js
+++ src/screens/TrainerScreen.js
@@ -7,11 +7,18 @@ import Account from '../components/Account'
 
 import firebase from 'react-native-firebase';
 
+// make date in format 2019-05-31
 const d = new Date();
-const today = d.getFullYear() + '-' + d.getMonth()+1 + '-' + d.getDate()
+const today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate()
 
+// this is the library of questions. add freely to them – how many there are doesn't matter!
 const questions = [
-  'What are three things you\'re thankful for today?'
+  'What made you smile today?',
+  'What are three things you\'re thankful for today?',
+  'What are 2 things you have today that you would never have dreamed of having 10 years ago?',
+  'What good things have you achieved that your childhood self would have never believed?',
+  'What\'s better about your life now that it was a decade ago?',
+  'What\'s 2 things you admire about someone you dislike?'
 ]
 
 export default class TrainerScreen extends React.Component {
@@ -25,8 +32,8 @@ export default class TrainerScreen extends React.Component {
     this.state = { 
         user: {},
         todaysPositivity: '',
-        pastPositivity: null,
-        question: 'What are three things you\'re thankful for today?',
+        todaysQuestion: '',
+        pastPositivity: [],
         saved: false
        };
     var timeout = null;
@@ -36,13 +43,17 @@ export default class TrainerScreen extends React.Component {
   componentDidMount(){
     firebase.analytics().logEvent('PositivityScreen_Loaded')
 
+    // GET USER
     AsyncStorage.getItem('user')
       .then(user =>  {
           userObject = JSON.parse(user)
           this.setState({user: userObject })
           // console.log(userObject.uid)
+          
+          // GET THEIR DATA
           firebase.database().ref('users/' + userObject.uid + '/').on('value', (snapshot) => {
-            // console.log(snapshot.val());
+            
+            // MAKE THE STATES
             this._makePositivityStates(snapshot.val())
           });
     })
@@ -82,63 +93,75 @@ export default class TrainerScreen extends React.Component {
     //if there are any unread badgets, remove them.
     firebase.notifications().setBadge(0)
 
-
-    // not sure if below two are necessary.
-    this.notificationDisplayedListener = firebase.notifications().onNotificationDisplayed((notification: Notification) => {
-            // Process your notification as required
-            // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
-        });
-    
-    this.notificationListener = firebase.notifications().onNotification((notification: Notification) => {
-        // Process your notification as required
-        // console.log('notif received')
-    });
-
-
     // ------------- ** END FIREBASE NOTIFICATION CODE ** -----
           
     
   }
 
 
-  storePositivity(user, positivity) {
-    // console.log(user.uid, positivity)
-    this.setState({todaysPositivity: positivity, saved: false})
+  _makePositivityStates(data){
+    var pastPositivity = []
+    var todaysPositivity = ''
+    var todaysQuestion = ''
+    
+    for (var key in data){
+      if (key === today) {
+        // if the item is today's positivity – push it to string
+        todaysPositivity = data[key].positivity
+        todaysQuestion = data[key].question
+      } else {
+        // else push it to past positivities array
+        pastPositivity.push({
+          positivity: data[key].positivity,
+          created: data[key].created,
+          question: data[key].question
+        }) 
+      }
+    }
+
+    // if there's no active question even after checked the server (ie. today is still blank), create a question for today and save it to server.
+    if (todaysQuestion === ''){ 
+      todaysQuestion = this.getRandomQuestion()
+      // save the qeustion to the server so that it doesn't change any more today
+      firebase.database().ref('users/' + this.state.user.uid + '/' + today ).set({
+        question: todaysQuestion
+      });
+    }
+
+    // save all to state
+    this.setState({
+        pastPositivity: pastPositivity.reverse(),
+        todaysPositivity: todaysPositivity, 
+        todaysQuestion: todaysQuestion  
+      })
+
+  }
+
+  getRandomQuestion(){
+    //make a random number between one and the length of the items in the question array
+    randomNumber = Math.floor(Math.random() * questions.length)
+    return questions[randomNumber]
+  }
+
+  storePositivity(text) {
+    // console.log(user.uid, text)
+    this.setState({todaysPositivity: text, saved: false})
 
     clearTimeout(this.timeout);
 
     // Make a new timeout set to go off in 800ms
     this.timeout = setTimeout(() => {
-        if (user != null) {
-          firebase.database().ref('users/' + user.uid + '/' + today ).set({
-            positivity: positivity,
-            created: today
+        if (this.state.user != null) {
+          firebase.database().ref('users/' + this.state.user.uid + '/' + today ).set({
+            positivity: this.state.todaysPositivity,
+            created: today,
+            question: this.state.todaysQuestion
           });
         }
         this.setState({saved: true})
         this.timeout = setTimeout(() => { this.setState({saved: false}) }, 1000)
     }, 2000);
     
-  }
-
-
-  _makePositivityStates(data){
-    var pastPositivity = []
-    var todaysPositivity = " "
-    
-    for (var key in data){
-      key !== today ?
-      
-      // push past positivities to array
-      pastPositivity.push({
-        positivity: data[key].positivity,
-        created: data[key].created
-      }) 
-      :
-      // push today's positivitiy to string
-      todaysPositivity = data[key].positivity
-    }
-    this.setState({pastPositivity: pastPositivity.reverse(), todaysPositivity: todaysPositivity  })
   }
 
   
@@ -155,33 +178,34 @@ export default class TrainerScreen extends React.Component {
               <View style={styles.element}>
                 <Text style={styles.center}>Hi, {this.state.user.displayName}!</Text>
                 <Text></Text>
-                <Text>
-                  {this.state.question}
-                  {' '}
-                  {this.state.saved ? <Text>💾</Text> : null }
-                </Text>
+                {this.state.todaysQuestion ? 
+                  <Text>
+                    {this.state.todaysQuestion}
+                    {' '}
+                    {this.state.saved ? <Text>💾</Text> : null }
+                  </Text>
+                : null }
                 <TextInput 
                   multiline={true}
                   value={this.state.todaysPositivity}
-                  onChangeText={(text) => this.storePositivity(this.state.user, text)}
+                  placeholder={'Answer here...'}
+                  onChangeText={(text) => this.storePositivity(text)}
                   style={styles.textInput}
-                  // autoFocus={true}
                   />
                 
               </View> 
 
 
-              { this.state.data ?
+              { this.state.pastPositivity.length > 0 ?
                 <View style={styles.element}>
-                  <Text>....</Text>
-                  <Text>Past things you've been thankful for:</Text>
+                  <Text>Past Positivities:</Text>
                 </View>
                : null }
 
-              { this.state.pastPositivity ? 
+              { this.state.pastPositivity.length > 0 ? 
                   this.state.pastPositivity.map((item, key) => 
                   <View style={styles.element} key={key}>
-                    <Text>On {item.created}, you were thankful for:</Text>
+                    <Text>{item.question}</Text>
                     <Text style={styles.center}>{item.positivity}</Text>
                   </View>
                   )
@@ -201,8 +225,6 @@ const styles = StyleSheet.create({
   },
   element:{
     margin: 20,
-    flex: 1,
-    alignItems: 'flex-start'
   },
   textInput:{
     borderWidth: 1,
